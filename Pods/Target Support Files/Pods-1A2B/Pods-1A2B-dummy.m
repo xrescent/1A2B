7c6bc78e503c4a361d6a23392e8da60297d9a9ec
michaelrevlis <michaelrevlis@gmail.com>
2016-11-05 12:11:41 +0800

    add firebase

---
+++ Pods/Target Support Files/Pods-1A2B/Pods-1A2B-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_1A2B : NSObject
+@end
+@implementation PodsDummy_Pods_1A2B
+@end
---
+++ Pods/Target Support Files/Pods-1A2B/Pods-1A2B-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_1A2BVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_1A2BVersionString[];
+
